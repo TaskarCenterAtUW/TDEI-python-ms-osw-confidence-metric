8bdce699de3bc0f13f97c710ee108262c7110f7a
Naresh Kumar D <susrisha@gmail.com>
2023-11-07 14:25:58 +0530

    Files modified.

--- src/models/confidence_request.py
+++ src/models/confidence_request.py
@@ -1,7 +1,7 @@
 from dataclasses import dataclass
 
 @dataclass
-class ConfidenceRequested():
+class ConfidenceRequest:
     jobId: str
     data_file:str
     meta_file:str
---
+++ tests/unit_tests/test_confidence_request.py
@@ -0,0 +1,9 @@
+# Tests the confidence request class
+
+from src.models.confidence_request import ConfidenceRequest
+import unittest
+
+#TODO: Have to write the confidence request test cases
+class TestConfidenceRequest(unittest.TestCase):
+    def setUp(self):
+        pass
